c8236facc44c36a4f927d129cfeaf3f098240735
Parshuram1011 <ZCOERRA_S1511023_2023_24@zealeducation.com>
2025-09-14 09:54:32 +0530

    Add files via upload

---
+++ else1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    if (1) {
+        printf("This code will compile.\n");
+    } else {
+        printf("This code will not compile.\n");
+    }
+    
+    return 0;
+}
