2bca30b84fd99ccbd83cb1806819a498f73b86dd
Parshuram1011 <ZCOERRA_S1511023_2023_24@zealeducation.com>
2025-10-02 18:55:07 +0530

    Hello robotics

---
+++ assignm1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    char N[50];
+    printf("Enter your name: ");
+    scanf("%s", N);
+    printf("Hello, Robotic.!,");
+    printf("Welcome %s ", N);
+    return 0;
+}
